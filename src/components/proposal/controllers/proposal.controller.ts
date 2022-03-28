798f234cd901e0d3d967ea99312a8bc4ac0875ca
thub1310577 <thub1310577@gmail.com>
2022-03-28 16:29:40 +0700

    [Aura][ExplorerApi][DEV][ThuNPA] 15 proposals view list of proposals

--- src/components/proposal/controllers/proposal.controller.ts
+++ src/components/proposal/controllers/proposal.controller.ts
@@ -1,4 +1,4 @@
-import { ClassSerializerInterceptor, Controller, Get, HttpStatus, UseInterceptors } from "@nestjs/common";
+import { ClassSerializerInterceptor, Controller, Get, HttpStatus, Param, UseInterceptors } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AkcLogger, BaseApiResponse, ReqContext, RequestContext, SwaggerBaseApiResponse } from "../../../shared";
 import { ProposalOutput } from "../dtos/proposal-output.dto";
@@ -25,10 +25,29 @@ export class ProposalController {
     @UseInterceptors(ClassSerializerInterceptor)
     async getProposals(
         @ReqContext() ctx: RequestContext
-    ): Promise<BaseApiResponse<ProposalOutput[]>> {
+    ): Promise<any> {
         this.logger.log(ctx, `${this.getProposals.name} was called!`);
         const { proposals, count } = await this.proposalService.getProposals(ctx);
 
         return { data: proposals, meta: { count } };
+    }
+
+    @Get(':proposalId/votes/:voter')
+    @ApiOperation({
+        summary: 'Get list proposals',
+    })
+    @ApiResponse({
+        status: HttpStatus.OK
+    })
+    @UseInterceptors(ClassSerializerInterceptor)
+    async getProposalVote(
+        @ReqContext() ctx: RequestContext,
+        @Param('proposalId') proposalId: string,
+        @Param('voter') voter: string,
+    ): Promise<any> {
+        this.logger.log(ctx, `${this.getProposalVote.name} was called!`);
+        const proposalVote = await this.proposalService.getProposalVote(ctx, proposalId, voter);
+
+        return { data: proposalVote, meta: {} };
     }
 }
