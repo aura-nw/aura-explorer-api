637f2f6cdca129dfb6684c94816ff47c811d3c86
thub1310577 <thub1310577@gmail.com>
2022-04-22 15:18:57 +0700

    [Aura][ExplorerApi][DEV][ThuNPA] 30 staking delegate a validator fix issue 167

--- src/components/validator/repositories/validator.repository.ts
+++ src/components/validator/repositories/validator.repository.ts
@@ -25,8 +25,9 @@ export class ValidatorRepository extends Repository<Validator> {
      */
     async getDelegators(operatorAddr: string, delegatorAddr: string) {
         const sql = `SELECT val.title, val.operator_address, val.acc_address, val.commission, val.status, val.jailed,
-                    (SELECT del.delegator_address FROM delegations del WHERE del.delegator_address=? AND del.validator_address = val.operator_address limit 1) AS staking_address
+                    (SELECT del.delegator_address FROM delegations del WHERE del.delegator_address=? AND del.validator_address = val.operator_address limit 1) AS staking_address,
+                    IFNULL((SELECT SUM(amount) FROM delegations WHERE validator_address = val.operator_address AND delegator_address = ?), 0) AS amount_staked
             FROM validators val WHERE val.OPERATOR_ADDRESS!=? AND val.status=3`;
-        return await this.query(sql, [delegatorAddr, operatorAddr]);
+        return await this.query(sql, [delegatorAddr, delegatorAddr, operatorAddr]);
     }
 }
