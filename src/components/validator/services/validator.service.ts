8d0d2acc649e196feb2e0275d16d971837e9e493
lthao1986 <lthao1986@gmail.com>
2022-04-22 16:43:26 +0700

    Delegators table displayed incorrect data

--- src/components/validator/services/validator.service.ts
+++ src/components/validator/services/validator.service.ts
@@ -281,10 +281,10 @@ export class ValidatorService {
    * @param offset 
    */
   async getDelegatorByValidatorAddr(ctx: RequestContext, validatorAddress: string,params : DelegatorByValidatorAddrParamsDto) {
-    const {pageResults, count} = await this.validatorRepository.getDelegatorByValidatorAddr(validatorAddress, params.limit, params.offset);
+    const {pageResults, total} = await this.validatorRepository.getDelegatorByValidatorAddr(validatorAddress, params.limit, params.offset);
      const responses = plainToClass(DelegatorByValidatorAddrOutputDto, pageResults, {
       excludeExtraneousValues: true,
     });
-    return { data: responses, count};
+    return { data: responses, total};
   }
 }
