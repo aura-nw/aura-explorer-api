36f8b5f625840673d6fbefb4c2351d9d0f2f8a57
thub1310577 <thub1310577@gmail.com>
2022-05-23 14:12:17 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] sync incorrect data redelegate
    (cherry picked from commit 5a27fcd2e1d91d794cacacaa7b4e3ba15470bdbb)

---
+++ src/migrations/1653286772136-delete-unique-index-delegator_rewards.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class deleteUniqueIndexDelegatorRewards1653286772136 implements MigrationInterface {
+    name = 'deleteUniqueIndexDelegatorRewards1653286772136'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX \`tx_hash\` ON \`delegator_rewards\``);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE UNIQUE INDEX \`tx_hash\` ON \`delegator_rewards\` (\`tx_hash\`)`);
+    }
+
+}
---
+++ src/migrations/1653287066034-insert-miss-data-delegator_rewards.ts
@@ -0,0 +1,34 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class insertMissDataDelegatorRewards1653287066034 implements MigrationInterface {
+    name = 'insertMissDataDelegatorRewards1653287066034'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`delegator_rewards\` CHANGE \`amount\` \`amount\` bigint NOT NULL`);
+        await queryRunner.query(`INSERT INTO delegator_rewards (tx_hash, delegator_address, validator_address, amount)
+            SELECT t1.tx_hash, t1.delegator_address, t1.validator_address, t2.amount FROM ( 
+            (SELECT tx_hash, delegator_address, validator_address 
+                FROM delegations WHERE amount < 0) t1
+            INNER JOIN
+            (SELECT tx_hash, REPLACE(REPLACE(JSON_EXTRACT(raw_log, '$[0].events[4].attributes[2].value'), 'uaura', ''), '\"', '') AS amount
+                FROM transactions) t2
+            ON t1.tx_hash = t2.tx_hash
+            ) WHERE t1.tx_hash IN (
+                SELECT tx_hash from delegations 
+                WHERE type IN ('Redelegate')
+                    AND tx_hash NOT IN (SELECT tx_hash FROM delegator_rewards)
+            )
+            UNION ALL
+            (SELECT tx_hash, delegator_address, validator_address, 0
+                FROM delegations 
+                WHERE tx_hash IN (
+                    SELECT tx_hash 
+                    FROM delegations 
+                    WHERE type IN ('Redelegate')
+                        AND tx_hash NOT IN (SELECT tx_hash FROM delegator_rewards)
+                ) AND amount > 0)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+
+}
