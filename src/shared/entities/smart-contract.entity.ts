fe3fc7b208db45ee5f7cdee44d8a6019be55fd11
AnDQK <AnDQK@fsoft.com.vn>
2022-05-23 11:03:46 +0700

    update smart contract table and entity

--- src/shared/entities/smart-contract.entity.ts
+++ src/shared/entities/smart-contract.entity.ts
@@ -4,14 +4,23 @@ import { BaseEntityIncrementId } from "./base/base.entity";
 @Entity('smart_contracts')
 export class SmartContract extends BaseEntityIncrementId {
     @Column()
+    height: number;
+
+    @Column()
     contract_address: string;
 
     @Column()
     creator_address: string;
 
     @Column()
+    contract_hash: string;
+
+    @Column()
     schema: string;
 
     @Column()
     url: string;
+
+    @Column()
+    contract_verification: string;
 }
