52b3262d1cb70caba095f39de7cb0f70f5d1eae9
thub1310577 <thub1310577@gmail.com>
2022-06-01 10:27:04 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] get contract detail

--- src/shared/entities/tag.entity.ts
+++ src/shared/entities/tag.entity.ts
@@ -4,12 +4,15 @@ import { BaseEntityIncrementId } from "./base/base.entity";
 @Entity('tags')
 @Unique(['account_address', 'contract_address'])
 export class Tag extends BaseEntityIncrementId {
-    @Column()
+    @Column({update: false})
     account_address: string;
 
-    @Column()
+    @Column({update: false})
     contract_address: string;
 
     @Column()
     tag: string;
+
+    @Column()
+    note: string;
 }
