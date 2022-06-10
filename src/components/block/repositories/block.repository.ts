8abc9d30eca2a12946745af5e56e43ac53bc3873
lthao1986 <lthao1986@gmail.com>
2022-06-10 08:57:03 +0700

    1/ Add index for operator_address column of blocks table
    2/ Update logic get valedator detail

--- src/components/block/repositories/block.repository.ts
+++ src/components/block/repositories/block.repository.ts
@@ -3,4 +3,13 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Block } from '../../../shared';
 
 @EntityRepository(Block)
-export class BlockRepository extends Repository<Block> {}
+export class BlockRepository extends Repository<Block> {
+    public getMinHeight = async (operator_address: string) =>{
+        const result = await this.createQueryBuilder()
+        .select("MIN(height) AS height")
+        .where({operator_address })
+        .getRawOne();
+
+        return result;
+    }
+}
