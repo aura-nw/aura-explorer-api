9f1e4996bd13be02fbf32cce8e7e22254c38c568
thub1310577 <thub1310577@gmail.com>
2022-07-12 11:23:45 +0700

    [Aura][ExplorerApi][Halo][ThuNPA] create table smart_contract_codes

---
+++ src/migrations/1656575274006-add-column-creator-to-smart-contract-codes-table.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addColumnCreatorToSmartContractCodesTable1656575274006 implements MigrationInterface {
+    name = 'addColumnCreatorToSmartContractCodesTable1656575274006'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`smart_contract_codes\` ADD \`creator\` varchar(255) NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`smart_contract_codes\` DROP COLUMN \`creator\``);
+    }
+
+}
