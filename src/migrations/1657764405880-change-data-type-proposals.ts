41b5cace74bc812e0cf5c7076cd50c71424b8db4
thub1310577 <thub1310577@gmail.com>
2022-07-14 10:15:03 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] change column type table proposals

--- src/migrations/1657764405880-change-data-type-proposals.ts
+++ src/migrations/1657764405880-change-data-type-proposals.ts
@@ -4,13 +4,13 @@ export class changeDataTypeProposals1657764405880 implements MigrationInterface 
     name = 'changeDataTypeProposals1657764405880'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_turnout\` \`pro_turnout\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_votes_yes\` \`pro_votes_yes\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_votes_abstain\` \`pro_votes_abstain\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_votes_no\` \`pro_votes_no\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_votes_no_with_veto\` \`pro_votes_no_with_veto\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-        await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_total_deposits\` \`pro_total_deposits\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
-		await queryRunner.query(`ALTER TABLE \`proposals\`  CHANGE COLUMN \`pro_participation_rate\` \`pro_participation_rate\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_turnout\` \`pro_turnout\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_votes_yes\` \`pro_votes_yes\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_votes_abstain\` \`pro_votes_abstain\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_votes_no\` \`pro_votes_no\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_votes_no_with_veto\` \`pro_votes_no_with_veto\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+        await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_total_deposits\` \`pro_total_deposits\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
+		await queryRunner.query(`ALTER TABLE \`proposals\` CHANGE COLUMN \`pro_participation_rate\` \`pro_participation_rate\` DECIMAL(30,6) NOT NULL DEFAULT '0.00000000'`);
     }
 
     public async down(_queryRunner: QueryRunner): Promise<void> {
