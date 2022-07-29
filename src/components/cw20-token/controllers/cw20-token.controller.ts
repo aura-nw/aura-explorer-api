884c6f7b340dc8aace0aa2faf2254a0bdf5436eb
thub1310577 <thub1310577@gmail.com>
2022-07-29 16:02:15 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] cw20 tokens list

--- src/components/cw20-token/controllers/cw20-token.controller.ts
+++ src/components/cw20-token/controllers/cw20-token.controller.ts
@@ -1,6 +1,7 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { AkcLogger } from "../../../shared";
+import { Body, CacheInterceptor, ClassSerializerInterceptor, Controller, HttpStatus, Post, UseInterceptors } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { AkcLogger, ReqContext, RequestContext } from "../../../shared";
+import { Cw20TokenParamsDto } from "../dtos/cw20-token-params.dto";
 import { Cw20TokenService } from "../services/cw20-token.service";
 
 @ApiTags('cw20-tokens')
@@ -11,5 +12,17 @@ export class Cw20TokenController {
         private readonly logger: AkcLogger,
     ) {
         this.logger.setContext(Cw20TokenController.name);
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'Get list cw20 tokens' })
+    @ApiResponse({ status: HttpStatus.OK })
+    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(CacheInterceptor)
+    async getCw20Tokens(@ReqContext() ctx: RequestContext, @Body() request: Cw20TokenParamsDto): Promise<any> {
+        this.logger.log(ctx, `${this.getCw20Tokens.name} was called!`);
+        const { tokens, count } = await this.cw20TokenService.getCw20Tokens(ctx, request);
+
+        return { data: tokens, meta: { count } };
     }
 }
--- src/components/cw20-token/services/cw20-token.service.ts
+++ src/components/cw20-token/services/cw20-token.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from "@nestjs/common";
-import { AkcLogger } from "../../../shared";
+import { AkcLogger, RequestContext } from "../../../shared";
+import { Cw20TokenParamsDto } from "../dtos/cw20-token-params.dto";
+import * as appConfig from '../../../shared/configs/configuration';
 
 @Injectable()
 export class Cw20TokenService {
+    private api;
+    private indexerUrl;
+    private indexerChainId;
+
     constructor(
         private readonly logger: AkcLogger
     ) {
         this.logger.setContext(Cw20TokenService.name);
+        const appParams = appConfig.default();
+        this.api = appParams.node.api;
+        this.indexerUrl = appParams.indexer.url;
+        this.indexerChainId = appParams.indexer.chainId;
+    }
+
+    async getCw20Tokens(ctx: RequestContext, request: Cw20TokenParamsDto): Promise<any> {
+        this.logger.log(ctx, `${this.getCw20Tokens.name} was called!`);
+        
+
+        return { contracts: [], count: 0 };
     }
 }
