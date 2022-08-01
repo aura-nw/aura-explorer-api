123f09e2a58013048f131703c7afe05dc0b9d7c0
thub1310577 <thub1310577@gmail.com>
2022-08-01 17:22:36 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] cw20 tokens list

--- src/migrations/1659321056909-update-table-token-contracts.ts
+++ src/migrations/1659321056909-update-table-token-contracts.ts
@@ -4,9 +4,7 @@ export class updateTableTokenContracts1659321056909 implements MigrationInterfac
     name = 'updateTableTokenContracts1659321056909'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // await queryRunner.query(`DROP INDEX \`contract_address\` ON \`token_contracts\``);
         await queryRunner.query(`ALTER TABLE \`token_contracts\` ADD \`asset_id\` VARCHAR(255) NOT NULL`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`token_contracts_idx_asset_id\` ON \`token_contracts\` (\`asset_id\`)`);
         await queryRunner.query(`ALTER TABLE \`token_contracts\` CHANGE \`decimal\` \`decimals\` INT NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`token_contracts\` CHANGE \`max_total_supply\` \`total_supply\` DECIMAL(30, 6) NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`token_contracts\` CHANGE \`description\` \`description\` TEXT NOT NULL`);
