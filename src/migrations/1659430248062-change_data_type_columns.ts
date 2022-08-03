80ea4117abd732e712c00cd5ae9f8fab56e7e229
Thanh Hao <lthao1986@gmail.com>
2022-08-03 11:19:04 +0700

    Changae data type columns

---
+++ src/migrations/1659430248062-change_data_type_columns.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class changeDataTypeColumns1659430248062 implements MigrationInterface {
+    name = 'changeDataTypeColumns1659430248062'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`transactions\` CHANGE \`data\` \`data\` text CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL`);
+        await queryRunner.query(`ALTER TABLE \`transactions\` CHANGE \`raw_log_data\` \`raw_log_data\` text CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL`);
+        await queryRunner.query(`ALTER TABLE \`validators\` CHANGE \`details\` \`details\` text CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL`);
+        await queryRunner.query(`ALTER TABLE \`history_proposals\` CHANGE \`description\` \`description\` text CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
