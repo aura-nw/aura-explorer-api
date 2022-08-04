5e0a860aed0c6c058407ab308146d591ab118b3f
thub1310577 <thub1310577@gmail.com>
2022-08-04 09:36:57 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] cw721 token nfts list/detail

--- src/components/cw721-token/services/cw721-token.service.ts
+++ src/components/cw721-token/services/cw721-token.service.ts
@@ -3,12 +3,15 @@ import { TokenContractRepository } from "../../../components/contract/repositori
 import { AkcLogger, CONTRACT_TYPE, RequestContext } from "../../../shared";
 import { Cw721TokenParamsDto } from "../dtos/cw721-token-params.dto";
 import { Like } from "typeorm";
+import { NftParamsDto } from "../dtos/nft-params.dto";
+import { NftRepository } from "../repositories/nft.repository";
 
 @Injectable()
 export class Cw721TokenService {
     constructor(
         private readonly logger: AkcLogger,
         private tokenContractRepository: TokenContractRepository,
+        private nftRepository: NftRepository
     ) {
         this.logger.setContext(Cw721TokenService.name);
     }
@@ -44,5 +47,28 @@ export class Cw721TokenService {
         });
 
         return token ? token : null;
+    }
+
+    async getNftsByContractAddress(ctx: RequestContext, contractAddress: string, request: NftParamsDto): Promise<any> {
+        this.logger.log(ctx, `${this.getNftsByContractAddress.name} was called!`);
+        const [nfts, count] = await this.nftRepository.findAndCount({
+            where: {
+                contract_address: contractAddress,
+                ...(request?.token_id && { token_id: request.token_id }),
+                ...(request?.owner && { owner: request.owner })
+            },
+            order: { updated_at: 'DESC' },
+            take: request.limit,
+            skip: request.offset
+        });
+
+        return { nfts: nfts, count: count };
+    }
+
+    async getNftByContractAddressAndTokenId(ctx: RequestContext, contractAddress: string, tokenId: string): Promise<any> {
+        this.logger.log(ctx, `${this.getNftByContractAddressAndTokenId.name} was called!`);
+        const nfts = await this.nftRepository.getNftByContractAddressAndTokenId(contractAddress, tokenId);
+
+        return nfts.length > 0 ? nfts[0] : null;
     }
 }
