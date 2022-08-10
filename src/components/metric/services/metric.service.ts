5ebb2ba3071268c15a6fa5ca10dc6813569e4472
Thanh Hao <lthao1986@gmail.com>
2022-08-10 09:07:28 +0700

    Update logic get data from influxdb

--- src/components/metric/services/metric.service.ts
+++ src/components/metric/services/metric.service.ts
@@ -49,12 +49,46 @@ export class MetricService {
       `calling ${TransactionRepository.name}.createQueryBuilder`,
     );
 
+    // Create start, stop position to get data fron influxdb
+    let stop = new Date();
+    const currentMinutes = stop.getMinutes();
     timezone = timezone * (-1);
+    stop.setSeconds(0, 0);
     const hours = (timezone > 0) ? Math.round(timezone / 60) : 0;
-    const { amount, step, fluxType } = buildCondition(range);
-    const startTime = `-${amount}${fluxType}`;
-    const queryStep = `${step}${fluxType}`;
-    const results = await this.influxDbClient.sumData('blocks_measurement', startTime, queryStep, 'num_txs') as MetricOutput[];
+
+    let start: Date = new Date();
+    let queryStep = ``;
+    if (range === Range.minute) {
+      start.setMinutes(-60);
+      queryStep = `1m`;
+
+    } else {
+      stop.setMinutes(timezone);
+      stop.setMinutes(stop.getMinutes() + currentMinutes);
+
+      start.setMinutes(timezone);
+      start.setMinutes(start.getMinutes() + currentMinutes);
+
+      switch (range) {
+        case Range.month:
+          start.setMonth(-12);
+          start.setUTCHours(0, 0, 0, 0);
+          queryStep = `1mo`;
+          break;
+        case Range.day:
+          start.setDate(-30);
+          start.setUTCHours(0, 0, 0, 0);
+          queryStep = `1d`;
+          break;
+        case Range.hour:
+          start.setHours(-24);
+          start.setSeconds(0, 0);
+          queryStep = `1h`;
+          break;
+      }
+    }
+
+    let results: MetricOutput[] = await this.influxDbClient.sumData('blocks_measurement', start.toISOString(), stop.toISOString(), queryStep, 'num_txs', timezone) as MetricOutput[];
     const series = generateSeries(range, hours);
     const metricData = mergeByProperty(results, series);    
     return metricData.map((item) => {
