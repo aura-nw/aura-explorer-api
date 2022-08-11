ebf778a6ba1e9657ef2fad9094ca7f4e0013d609
Thanh Hao <lthao1986@gmail.com>
2022-08-11 16:26:46 +0700

    [Thanhhao1986][Dev] Fix search smart contract (#351)
    * Improve performance for system
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Update logic get data from influxdb
    
    * Merge code from develop
    
    * Fix bug serche smart contract

--- src/components/contract/repositories/token-contract.repository.ts
+++ src/components/contract/repositories/token-contract.repository.ts
@@ -1,9 +1,9 @@
 import { InjectRepository } from "@nestjs/typeorm";
-import { CONTRACT_TRANSACTION_EXECUTE_TYPE, CONTRACT_TRANSACTION_TYPE, TokenContract } from "../../../shared";
-import { EntityRepository, ObjectLiteral, Repository } from "typeorm";
-import { TokenTransactionParamsDto } from "../../../components/cw20-token/dtos/token-transaction-params.dto";
+import { EntityRepository, FindManyOptions, ObjectLiteral, Raw, Repository } from "typeorm";
 import { Cw20TokenByOwnerParamsDto } from "../../../components/cw20-token/dtos/cw20-token-by-owner-params.dto";
+import { TokenTransactionParamsDto } from "../../../components/cw20-token/dtos/token-transaction-params.dto";
 import { NftByOwnerParamsDto } from "../../../components/cw721-token/dtos/nft-by-owner-params.dto";
+import { CONTRACT_TRANSACTION_EXECUTE_TYPE, CONTRACT_TRANSACTION_TYPE, CONTRACT_TYPE, TokenContract } from "../../../shared";
 
 @EntityRepository(TokenContract)
 export class TokenContractRepository extends Repository<TokenContract> {
@@ -48,12 +48,40 @@ export class TokenContractRepository extends Repository<TokenContract> {
         let sqlLimit = " LIMIT ? OFFSET ?";
         params.push(request.limit);
         params.push(request.offset);
-    
+
         result[0] = await this.query(sqlSelect + sql + sqlLimit, params);
         result[1] = await this.query(sqlCount + sql, params);
         return result;
     }
 
+    async getDataTokens(type: CONTRACT_TYPE, keyword: string, limit: number, offset: number) {
+        let condition: FindManyOptions<TokenContract> = {
+            where: {
+                type: CONTRACT_TYPE.CW20,
+            },
+            order: { updated_at: 'DESC' },
+        }
+
+        if (keyword) {
+            condition.where = [
+                {
+                    type: type,
+                    ...(keyword && { contract_address: Raw(() => ` LOWER(contract_address) LIKE :keyword`, { keyword: `%${keyword}%`.toLowerCase() }) }),
+                },
+                {
+                    type: type,
+                    ...(keyword && { contract_address: Raw(() => ` LOWER(name) LIKE :keyword`, { keyword: `%${keyword}%`.toLowerCase() }) })
+                }
+            ]
+        }
+        if (limit > 0) {
+            condition['take'] = limit;
+            condition['skip'] = offset;
+        }
+        const [tokens, count] = await this.findAndCount(condition);
+        return { tokens: tokens, count: count };
+    }
+    
     async getCw20TokensByOwner(request: Cw20TokenByOwnerParamsDto) {
         let result = [];
         let params = [];
