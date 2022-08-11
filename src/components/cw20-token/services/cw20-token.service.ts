07357e4ba8c3afbf5034f192c718131f0082bae5
thub1310577 <thub1310577@gmail.com>
2022-08-11 11:11:13 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] cw20/cw721 tokens list by owner

--- src/components/cw20-token/services/cw20-token.service.ts
+++ src/components/cw20-token/services/cw20-token.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from "@nestjs/common";
-import { AkcLogger, CONTRACT_TYPE, RequestContext } from "../../../shared";
+import { AkcLogger, CONTRACT_TYPE, INDEXER_API, RequestContext } from "../../../shared";
 import { Cw20TokenParamsDto } from "../dtos/cw20-token-params.dto";
 import { TokenContractRepository } from "../../../components/contract/repositories/token-contract.repository";
 import { Like } from "typeorm";
 import { TokenTransactionParamsDto } from "../dtos/token-transaction-params.dto";
+import * as appConfig from '../../../shared/configs/configuration';
+import * as util from 'util';
+import { ServiceUtil } from "../../../shared/utils/service.util";
+import { SmartContractRepository } from "../../../components/contract/repositories/smart-contract.repository";
 
 @Injectable()
 export class Cw20TokenService {
+    private appParams;
+    private indexerUrl;
+    private indexerChainId;
+
     constructor(
         private readonly logger: AkcLogger,
         private tokenContractRepository: TokenContractRepository,
+        private smartContractRepository: SmartContractRepository,
+        private serviceUtil: ServiceUtil
     ) {
         this.logger.setContext(Cw20TokenService.name);
+        this.appParams = appConfig.default();
+        this.indexerUrl = this.appParams.indexer.url;
+        this.indexerChainId = this.appParams.indexer.chainId;
     }
 
     async getCw20Tokens(ctx: RequestContext, request: Cw20TokenParamsDto): Promise<any> {
@@ -51,5 +64,36 @@ export class Cw20TokenService {
         const result = await this.tokenContractRepository.getListTokenTransactions(request);
 
         return { transactions: result[0], count: result[1][0].total };
+    }
+
+    async getTokensByOwner(ctx: RequestContext, accountAddress: string): Promise<any> {
+        this.logger.log(ctx, `${this.getTokensByOwner.name} was called!`);
+        const tokens = await this.serviceUtil.getDataAPI(`${this.indexerUrl}${util.format(INDEXER_API.GET_TOKENS_BY_OWNER, accountAddress, this.indexerChainId)}`, '', ctx);
+        const cw20Tokens = tokens.data.assets.CW20.asset;
+        const cw721Tokens = tokens.data.assets.CW721.asset;
+        if (cw20Tokens.length > 0) {
+            for (let i = 0; i < cw20Tokens.length; i++) {
+                const item = cw20Tokens[i];
+                const token = await this.tokenContractRepository.findOne({
+                    where: {
+                        contract_address: item.contract_address
+                    },
+                });
+                item.image = token ? token.image : '';
+            }
+        }
+        if (cw721Tokens.length > 0) {
+            for (let i = 0; i < cw721Tokens.length; i++) {
+                const item = cw721Tokens[i];
+                const contract = await this.smartContractRepository.findOne({
+                    where: {
+                        contract_address: item.contract_address
+                    },
+                });
+                item.name = contract ? contract.contract_name : '';
+            }
+        }
+
+        return tokens ? tokens : null;
     }
 }
