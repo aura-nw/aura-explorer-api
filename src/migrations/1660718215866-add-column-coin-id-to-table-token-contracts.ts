64404cb6bf3fa3f81e03ede9c9f4a21eb6c4a499
thub1310577 <thub1310577@gmail.com>
2022-08-17 13:40:27 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] cw20/cw721 add coin id

---
+++ src/migrations/1660718215866-add-column-coin-id-to-table-token-contracts.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addColumnCoinIdToTableTokenContracts1660718215866 implements MigrationInterface {
+    name = 'addColumnCoinIdToTableTokenContracts1660718215866'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`token_contracts\` ADD \`coin_id\` varchar(255) NOT NULL DEFAULT 'aura-network'`);
+        }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`token_contracts\` DROP COLUMN \`coin_id\``);
+    }
+
+}
