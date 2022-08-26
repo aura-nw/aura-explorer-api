deb5d4331250e9c3ff1ce188a953c5259ef64d01
nhphuc2411 <phucnguyen241193@gmail.com>
2022-08-26 12:54:16 +0700

    create index transaction table

--- src/migrations/1661483207924-transaction_create_index.ts
+++ src/migrations/1661483207924-transaction_create_index.ts
@@ -4,11 +4,10 @@ export class transactionCreateIndex1661483207924 implements MigrationInterface {
     name = 'transactionCreateIndex1661483207924'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE UNIQUE INDEX \`TRANSACTION_TYPE_IDX\` ON \`transactions\` (\`type\`)`);
+        await queryRunner.query(`ALTER TABLE \`transactions\`  ADD INDEX \`TRANSACTION_TYPE_IDX\` (\`type\` ASC) VISIBLE;`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX \`TRANSACTION_MESSAGES_IDX\` ON \`transactions\``);
     }
-
 }
