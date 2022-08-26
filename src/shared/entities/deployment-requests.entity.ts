782bedd3d42c7c40dd4e402f55918692503edbda
thub1310577 <thub1310577@gmail.com>
2022-08-26 13:27:02 +0700

    [Aura][ExplorerApi][Serenity][ThuNPA] create table deployment_requests

---
+++ src/shared/entities/deployment-requests.entity.ts
@@ -0,0 +1,80 @@
+import { Column, Entity } from "typeorm";
+import { BaseEntityIncrementId } from "./base/base.entity";
+
+@Entity('deployment_requests')
+export class DeploymentRequests extends BaseEntityIncrementId {
+    @Column()
+    request_id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    contract_description: string;
+
+    @Column()
+    project_name: string;
+
+    @Column()
+    official_project_website: string;
+
+    @Column()
+    official_project_email: string;
+
+    @Column()
+    project_sector: string;
+
+    @Column()
+    whitepaper: string;
+
+    @Column()
+    github: string;
+
+    @Column()
+    telegram: string;
+
+    @Column()
+    discord: string;
+
+    @Column()
+    facebook: string;
+
+    @Column()
+    twitter: string;
+    
+    @Column()
+    euphoria_code_id: number;
+
+    @Column()
+    mainnet_code_id: number;
+
+    @Column()
+    contract_hash: string;
+
+    @Column()
+    url: string;
+
+    @Column({ type: 'text' })
+    instantiate_msg_schema: string;
+
+    @Column({ type: 'text' })
+    query_msg_schema: string;
+
+    @Column({ type: 'text' })
+    execute_msg_schema: string;
+
+    @Column()
+    compiler_version: string;
+
+    @Column()
+    s3_location: string;
+
+    @Column()
+    status: string;
+
+    @Column()
+    reason: string;
+}
