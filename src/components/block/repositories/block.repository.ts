4d1a6bbf32eb46dacbced03e2910d65b0d444de2
Thanh Hao <lthao1986@gmail.com>
2022-08-29 14:04:57 +0700

    [Thanhhao1986][Dev] Get transaction of CW721 (#392)
    * Get Transactions of CW721 contract
    
    * Commit code CW721
    
    * Commit code search token contract CW721
    
    * Commit code cw721
    
    * Get transaction cw721
    
    * Get transaction of CW721
    
    * Commit code CW721

--- src/components/block/repositories/block.repository.ts
+++ src/components/block/repositories/block.repository.ts
@@ -4,13 +4,13 @@ import { Block } from '../../../shared';
 
 @EntityRepository(Block)
 export class BlockRepository extends Repository<Block> {
-    public getMinHeight = async (operator_address: string) =>{
+    public getMinHeight = async (operator_address: string) => {
         const result = await this.createQueryBuilder()
-        .select("height")
-        .where({operator_address })
-        .limit(1)
-        .addOrderBy("height")
-        .getRawOne();
+            .select("height")
+            .where({ operator_address })
+            .limit(1)
+            .addOrderBy("height")
+            .getRawOne();
 
         return result;
     }
@@ -22,7 +22,7 @@ export class BlockRepository extends Repository<Block> {
      * @param topBlock: Number of blocks to get
      * @returns Array<TextRow>
      */
-    async getBlockUptime(address: string, topBlock: number){
+    async getBlockUptime(address: string, topBlock: number) {
         const sql = `
         SELECT tmpBlock.height, tmpBlock.block_hash, (CASE WHEN tmpMiss.height = tmpBlock.height THEN 1 ELSE 0 END) isMissed FROM(
         SELECT height, block_hash, operator_address FROM blocks ORDER BY height DESC LIMIT ${topBlock}
@@ -31,7 +31,24 @@ export class BlockRepository extends Repository<Block> {
                 SELECT validator_address, height FROM missed_block 
                     WHERE validator_address =? ORDER BY height DESC LIMIT ${topBlock}
         ) AS tmpMiss ON tmpBlock.operator_address = tmpMiss.validator_address`;
-      
+
         return await this.query(sql, [address, address]);
     }
+
+    /**
+     * Get of validator bigin join
+     * @param operator_address 
+     * @returns 
+     */
+    public getHeightValidator = async (operator_address: string) => {
+        const result = await this.createQueryBuilder('blocks')
+            .select('blocks.height')
+            .innerJoin('transactions', 'tran', 'tran.blockId = blocks.id')
+            .where(`blocks.operator_address = :operator_address AND tran.type = '/cosmos.staking.v1beta1.MsgCreateValidator'`)
+            .setParameter('operator_address', operator_address)
+            .limit(1)
+            .addOrderBy('blocks.height')
+            .getRawOne();
+        return result;
+    };
 }
--- src/components/cw721-token/services/cw721-token.service.ts
+++ src/components/cw721-token/services/cw721-token.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { TokenContractRepository } from "../../../components/contract/repositories/token-contract.repository";
-import { AkcLogger, CONTRACT_TYPE, RequestContext } from "../../../shared";
+import { AkcLogger, CONTRACT_TRANSACTION_TYPE, CONTRACT_TYPE, RequestContext } from "../../../shared";
 import { Cw721TokenParamsDto } from "../dtos/cw721-token-params.dto";
 import { NftParamsDto } from "../dtos/nft-params.dto";
 import { NftRepository } from "../repositories/nft.repository";
 import { NftByOwnerParamsDto } from "../dtos/nft-by-owner-params.dto";
+import { TransactionRepository } from "../../transaction/repositories/transaction.repository";
+import { TokenTransactionRepository } from "../repositories/token-transaction.repository";
+import { any } from "joi";
+import { isNumber } from "class-validator";
+import { TokenCW721TransactionParasDto } from "../dtos/token-cw721-transaction-paras.dto";
 
 @Injectable()
 export class Cw721TokenService {
     constructor(
         private readonly logger: AkcLogger,
         private tokenContractRepository: TokenContractRepository,
-        private nftRepository: NftRepository
+        private nftRepository: NftRepository,
+        private transactionRepository: TransactionRepository,
+        private tokenTransactionRepository: TokenTransactionRepository,
     ) {
         this.logger.setContext(Cw721TokenService.name);
     }
@@ -19,7 +26,7 @@ export class Cw721TokenService {
     async getCw721Tokens(ctx: RequestContext, request: Cw721TokenParamsDto): Promise<any> {
         this.logger.log(ctx, `${this.getCw721Tokens.name} was called!`);
         const result = await this.tokenContractRepository.getCw721Tokens(request);
-        
+
         return { tokens: result[0], count: result[1][0].total };
     }
 
@@ -42,5 +49,62 @@ export class Cw721TokenService {
         const result = await this.tokenContractRepository.getNftsByOwner(request);
 
         return { tokens: result[0], count: result[1][0].total };
+    }
+
+    /**
+     * Get transactions by address
+     * @param address 
+     * @param type 
+     * @param limit 
+     * @param offset 
+     * @returns 
+     */
+    async getTransactionContract(req: TokenCW721TransactionParasDto): Promise<any> {
+        const [transactions, count] = await this.transactionRepository.getTransactionContract(req.contract_address, req.account_address, req.tx_hash, req.token_id, req.limit, req.offset);
+        if (transactions) {
+            const transactionBurn = await this.tokenTransactionRepository.getBurnByAddress(req.contract_address);
+            if (transactionBurn) {
+                transactions.forEach((item) => {
+                    const tokenId = this.getTokenId(item.messages);
+                    const filter = transactionBurn.filter(f => Number(f.token_id) === Number(tokenId)
+                        && Number(item.transaction_id) <= Number(f.last_id));
+                    if (filter) {
+                        item['disabled'] = true;
+                    } else {
+                        item['disabled'] = false;
+                    }
+                });
+            }
+        }
+        return [transactions, count];
+    }
+
+    /**
+     * Get transactions by Address and Token Id
+     * @param address 
+     * @param tokenType 
+     * @param token_id 
+     * @param limit 
+     * @param offset 
+     * @returns 
+     */
+    async viewNTFTransaction(address: string, token_id, limit: number, offset: number) {
+        return await this.transactionRepository.viewNTFTransaction(address, CONTRACT_TYPE.CW721, token_id, limit, offset);
+    }
+
+    /**
+     * Get token id from message
+     * @param message 
+     * @returns 
+     */
+    getTokenId(message: any) {
+        const msg = message[0];
+        if (msg.msg?.burn) {
+            return msg.msg?.burn.token_id;
+        } else if (msg.msg?.burn) {
+            return msg.msg?.mint.token_id;
+        } else if (msg.msg?.burn) {
+            return msg.msg?.transfer.token_id;
+        }
     }
 }
