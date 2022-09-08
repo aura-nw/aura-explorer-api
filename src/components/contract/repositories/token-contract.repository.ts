783878ef77eced1c85adb1a6c2803791cfaf322e
thub1310577 <thub1310577@gmail.com>
2022-09-08 11:47:32 +0700

    [Aura][ExplorerApi][Dev][ThuNPA] update logic sync cw721 tokens

--- src/components/contract/repositories/token-contract.repository.ts
+++ src/components/contract/repositories/token-contract.repository.ts
@@ -3,7 +3,7 @@ import { Cw721TokenParamsDto } from "../../../components/cw721-token/dtos/cw721-
 import { EntityRepository, FindManyOptions, Not, ObjectLiteral, Raw, Repository } from "typeorm";
 import { Cw20TokenByOwnerParamsDto } from "../../../components/cw20-token/dtos/cw20-token-by-owner-params.dto";
 import { NftByOwnerParamsDto } from "../../../components/cw721-token/dtos/nft-by-owner-params.dto";
-import { AURA_INFO, CONTRACT_TRANSACTION_EXECUTE_TYPE, CONTRACT_TRANSACTION_TYPE, CONTRACT_TYPE, TokenContract } from "../../../shared";
+import { AURA_INFO, CONTRACT_CODE_RESULT, CONTRACT_TRANSACTION_EXECUTE_TYPE, CONTRACT_TRANSACTION_TYPE, CONTRACT_TYPE, TokenContract } from "../../../shared";
 
 @EntityRepository(TokenContract)
 export class TokenContractRepository extends Repository<TokenContract> {
@@ -72,30 +72,34 @@ export class TokenContractRepository extends Repository<TokenContract> {
     async getCw721Tokens(request: Cw721TokenParamsDto) {
         let result = [];
         let params = [];
-        let sqlSelect: string = `SELECT tc.*,
-                (SELECT COUNT(id)
-            FROM transactions
-            WHERE contract_address = tc.contract_address
-                AND type = '${CONTRACT_TRANSACTION_TYPE.EXECUTE}'
-                AND timestamp > NOW() - INTERVAL 24 HOUR) AS transfers_24h,
-                (SELECT COUNT(id)
-            FROM transactions
-            WHERE contract_address = tc.contract_address
-                AND type = '${CONTRACT_TRANSACTION_TYPE.EXECUTE}'
-                AND timestamp > NOW() - INTERVAL 72 HOUR) AS transfers_3d`;
-        let sqlCount: string = `SELECT COUNT(tc.id) AS total`;
-        let sql: string = ` FROM token_contracts tc
-            WHERE tc.type = '${CONTRACT_TYPE.CW721}' AND tc.contract_address != '${AURA_INFO.CONNTRACT_ADDRESS}'`;
+        let sqlSelect: string = `SELECT sc.token_name AS name, sc.token_symbol AS symbol, sc.contract_address, sc.num_tokens,
+            (SELECT COUNT(id)
+                FROM token_transactions
+                WHERE contract_address = sc.contract_address
+                    AND created_at > NOW() - INTERVAL 24 HOUR) AS transfers_24h,
+            (SELECT COUNT(id)
+                FROM token_transactions
+                WHERE contract_address = sc.contract_address
+                    AND created_at > NOW() - INTERVAL 72 HOUR) AS transfers_3d,
+            (SELECT created_at
+                FROM token_transactions
+                WHERE contract_address = sc.contract_address
+                ORDER BY created_at DESC
+                LIMIT 1) AS upTime`;
+        let sqlCount: string = `SELECT COUNT(sc.id) AS total`;
+        let sql: string = ` FROM smart_contracts sc
+                INNER JOIN smart_contract_codes scc ON sc.code_id = scc.code_id AND scc.result = '${CONTRACT_CODE_RESULT.CORRECT}' AND scc.type = '${CONTRACT_TYPE.CW721}'
+            WHERE sc.is_minted = true`;
         if(request?.keyword) {
-            sql += ` AND (LOWER(tc.name) LIKE ? OR LOWER(tc.contract_address) LIKE ?)`
+            sql += ` AND (LOWER(sc.token_name) LIKE ? OR LOWER(sc.contract_address) LIKE ?)`
             params.push(`%${request.keyword.toLowerCase()}%`);
             params.push(`%${request.keyword.toLowerCase()}%`);
         }
         let sqlOrder = '';
         if (request?.sort_column && request?.sort_order) {
-            sqlOrder = ` ORDER BY ${request.sort_column} ${request.sort_order}, tc.updated_at DESC`;
+            sqlOrder = ` ORDER BY ${request.sort_column} ${request.sort_order}, upTime DESC`;
         } else {
-            sqlOrder = ` ORDER BY transfers_24h DESC, tc.updated_at DESC`;
+            sqlOrder = ` ORDER BY transfers_24h DESC, upTime DESC`;
         }
         let sqlLimit = '';
         if(request.limit > 0) {
@@ -139,10 +143,11 @@ export class TokenContractRepository extends Repository<TokenContract> {
     }
 
     async getTokenByContractAddress(contractAddress: string) {
-        let sql = `SELECT tc.*, sc.contract_verification, sc.tx_hash
-            FROM token_contracts tc
-                INNER JOIN smart_contracts sc ON tc.contract_address = sc.contract_address
-            WHERE tc.contract_address = ?`;
+        let sql = `SELECT sc.token_name AS name, sc.token_symbol AS symbol, sc.num_tokens,
+                sc.contract_address, sc.contract_verification, sc.tx_hash, scc.type
+            FROM smart_contracts sc
+                INNER JOIN smart_contract_codes scc ON sc.code_id = scc.code_id
+            WHERE sc.contract_address = ?`;
 
         return await this.repos.query(sql, [contractAddress]);
     }
