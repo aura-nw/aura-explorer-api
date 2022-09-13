69c692c8625ac942145f79734eff957f9a138e60
Do Quoc Khanh An <doquockhanhan@gmail.com>
2022-09-13 16:46:10 +0700

    update migration file alter table smart_contracts

--- src/migrations/1663051919960-updateContractTableAddVerifyTime.ts
+++ src/migrations/1663051919960-updateContractTableAddVerifyTime.ts
@@ -4,11 +4,13 @@ export class updateContractTableAddVerifyTime1663051919960 implements MigrationI
     name = 'updateContractTableAddVerifyTime1663051919960'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`smart_contracts\` ADD \`verified_at\` timestamp NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`smart_contracts\` ADD \`verified_at\` timestamp(6)`);
         await queryRunner.query(`ALTER TABLE \`smart_contracts\` RENAME COLUMN \`mainnet_code_id\` TO \`reference_code_id\``);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`smart_contracts\` RENAME COLUMN \`reference_code_id\` TO \`mainnet_code_id\``);
+        await queryRunner.query(`ALTER TABLE \`smart_contracts\` DROP COLUMN \`verified_at\``);
     }
 
 }
