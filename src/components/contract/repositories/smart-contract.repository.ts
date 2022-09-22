1d90b7a2346ed83cdaad4289ccbbaa337f37e00e
Thanh Hao <lthao1986@gmail.com>
2022-09-22 16:16:36 +0700

    [Thanhhao1986][Dev] Fix incorrect status (#451)
    * Commit code
    
    * Update logic search contract
    
    * Update search data paging
    
    * Update comment for function
    
    * Update Output getContractByCreator api
    
    * Update Output getContractByCreator api
    
    * Fix incorrect status
    
    * Fix incorrect status

--- src/components/contract/repositories/smart-contract.repository.ts
+++ src/components/contract/repositories/smart-contract.repository.ts
@@ -57,7 +57,8 @@ export class SmartContractRepository extends Repository<SmartContract> {
             .select('sm.code_id AS codeId')
             .distinct(true)
             .where(`sm.contract_verification != '${CONTRACT_STATUS.UNVERIFIED}'
-                AND sm.mainnet_upload_status NOT IN('${CONTRACT_STATUS.DEPLOYED}', '${CONTRACT_STATUS.TBD}', '${CONTRACT_STATUS.REJECTED}', '${CONTRACT_STATUS.NOT_REGISTERED}')
+                AND sm.mainnet_upload_status NOT IN('${CONTRACT_STATUS.DEPLOYED}', '${CONTRACT_STATUS.TBD}',
+                '${CONTRACT_STATUS.REJECTED}', '${CONTRACT_STATUS.NOT_REGISTERED}', ''${CONTRACT_STATUS.PENDING}'')
                 AND sm.creator_address=:creatorAddress`)
             .setParameter('creatorAddress', creatorAddress)
             .getRawMany();
