7527b517b50bc83db2b6eaaf278b195f0b123093
Thanh Hao <lthao1986@gmail.com>
2022-09-22 15:59:36 +0700

    [Thanhhao1986][Dev] Fix incorrect status (#450)
    * Commit code
    
    * Update logic search contract
    
    * Update search data paging
    
    * Update comment for function
    
    * Update Output getContractByCreator api
    
    * Update Output getContractByCreator api
    
    * Fix incorrect status

--- src/components/contract/repositories/smart-contract.repository.ts
+++ src/components/contract/repositories/smart-contract.repository.ts
@@ -1,6 +1,6 @@
-import { SmartContract } from "../../../shared/entities/smart-contract.entity";
-import { EntityRepository, ObjectLiteral, Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { EntityRepository, ObjectLiteral, Repository } from "typeorm";
+import { SmartContract } from "../../../shared/entities/smart-contract.entity";
 import { ContractParamsDto } from "../dtos/contract-params.dto";
 import { AURA_INFO, CONTRACT_STATUS, LENGTH, TokenContract } from "../../../shared";
 
@@ -48,16 +48,16 @@ export class SmartContractRepository extends Repository<SmartContract> {
     }
 
     /**
-    * Get list code id
-    * @param creatorAddress: Creator address
-    * @returns List code id (number[])
-    */
+  * Get list code id
+  * @param creatorAddress: Creator address
+  * @returns List code id (number[])
+  */
     async getCodeIds(creatorAddress: string) {
         return await this.createQueryBuilder('sm')
             .select('sm.code_id AS codeId')
             .distinct(true)
             .where(`sm.contract_verification != '${CONTRACT_STATUS.UNVERIFIED}'
-                AND sm.mainnet_upload_status NOT IN('${CONTRACT_STATUS.DEPLOYED}', '${CONTRACT_STATUS.TBD}')
+                AND sm.mainnet_upload_status NOT IN('${CONTRACT_STATUS.DEPLOYED}', '${CONTRACT_STATUS.TBD}', '${CONTRACT_STATUS.REJECTED}', '${CONTRACT_STATUS.NOT_REGISTERED}')
                 AND sm.creator_address=:creatorAddress`)
             .setParameter('creatorAddress', creatorAddress)
             .getRawMany();
@@ -85,7 +85,7 @@ export class SmartContractRepository extends Repository<SmartContract> {
             if (status === CONTRACT_STATUS.UNVERIFIED
                 || status === CONTRACT_STATUS.EXACT_MATCH
                 || status === CONTRACT_STATUS.SIMILAR_MATCH) {
-                    conditions += ` AND sm.contract_verification=:status`
+                conditions += ` AND sm.contract_verification=:status`
 
             } else {
                 conditions += ` AND sm.mainnet_upload_status=:status`
@@ -95,10 +95,11 @@ export class SmartContractRepository extends Repository<SmartContract> {
 
         let constracts = await this.createQueryBuilder('sm')
             .select(`sm.*, tokenContract.type, (CASE WHEN(
-                sm.contract_verification = '${CONTRACT_STATUS.UNVERIFIED}'
-                OR sm.contract_verification = '${CONTRACT_STATUS.SIMILAR_MATCH}'
-                OR sm.contract_verification = '${CONTRACT_STATUS.EXACT_MATCH}'
-            ) THEN sm.contract_verification ELSE sm.mainnet_upload_status END) AS status`)
+                sm.mainnet_upload_status = '${CONTRACT_STATUS.TBD}'
+                OR sm.mainnet_upload_status = '${CONTRACT_STATUS.DEPLOYED}'
+                OR sm.mainnet_upload_status = '${CONTRACT_STATUS.REJECTED}'
+                OR sm.mainnet_upload_status = '${CONTRACT_STATUS.PENDING}'
+            ) THEN sm.mainnet_upload_status ELSE sm.contract_verification END) AS status`)
             .innerJoin(TokenContract, 'tokenContract', 'tokenContract.contract_address=sm.contract_address')
             .distinct(true)
             .where(conditions)
