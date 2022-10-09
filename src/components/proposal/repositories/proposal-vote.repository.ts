fc800dd60d09c28aa245c4a488194f3f44cd36a0
Tam Bui <tam2391@gmail.com>
2022-10-09 13:39:44 +0700

    ﻿move and update (getCw721Tokens, getTokensByListContractAddress, getTokenByContractAddress ) from token-contract.repository to smart-contract.repository update roposal-vote.repository

--- src/components/proposal/repositories/proposal-vote.repository.ts
+++ src/components/proposal/repositories/proposal-vote.repository.ts
@@ -1,15 +1,23 @@
-import { ProposalVote } from "../../../shared/entities/proposal-vote.entity";
-import { EntityRepository, ObjectLiteral, Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
+import { ProposalVote } from '../../../shared/entities/proposal-vote.entity';
+import { EntityRepository, ObjectLiteral, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @EntityRepository(ProposalVote)
 export class ProposalVoteRepository extends Repository<ProposalVote> {
-    constructor( @InjectRepository(ProposalVote) private readonly repos: Repository<ObjectLiteral>) {
-        super();   
-    }
+  constructor(
+    @InjectRepository(ProposalVote)
+    private readonly repos: Repository<ObjectLiteral>,
+  ) {
+    super();
+  }
 
-    async countVoteByAddress(address: Array<string>){
-        const query: string = `SELECT voter, COUNT(1) AS countVote FROM proposal_votes where voter IN (?) GROUP BY  voter`;
-        return await this.query(query, [address]);
-    }
+  async countVoteByAddress(address: Array<string>) {
+    return await this.createQueryBuilder()
+      .select('voter, COUNT(1) AS countVote')
+      .where('voter IN (:...address)', {
+        address: address,
+      })
+      .orderBy('voter')
+      .getRawMany();
+  }
 }
