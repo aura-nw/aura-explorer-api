9787d0bd38511af2eb63b1805b358dcd6aeafd07
nthduc95 <nthduc95@gmail.com>
2022-12-13 17:15:58 +0700

    Change data type gas_used &gas_wanted column to big int at table block

--- src/migrations/1670925104021-change-data-type-block-table.ts
+++ src/migrations/1670925104021-change-data-type-block-table.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class migrationName1670925104021 implements MigrationInterface {
-    name = 'migrationName1670925104021'
+export class changeDataTypeBlockTable1670925104021 implements MigrationInterface {
+    name = 'changeDataTypeBlockTable1670925104021'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`blocks\` CHANGE COLUMN \`gas_used\` \`gas_used\` BIGINT NOT NULL DEFAULT '0'`);
