d8a6172081e11f810d6b10a683ee96149393450c
nthduc95 <nthduc95@gmail.com>
2023-02-07 09:04:18 +0700

    Add API to get SBT token list

---
+++ src/components/soulbound-token/dtos/soulbound-token-output.dto.ts
@@ -0,0 +1,15 @@
+import { Expose } from 'class-transformer';
+
+export class SoulboundTokenOutputDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  contract_address: number;
+
+  @Expose()
+  token_name: string;
+
+  @Expose()
+  minter_address: string;
+}
