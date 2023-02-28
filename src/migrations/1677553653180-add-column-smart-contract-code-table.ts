8fc218d1f410c0602f91405ecc43498996477727
nthduc95 <119287881+nthduc95@users.noreply.github.com>
2023-02-28 16:12:57 +0700

    [nthduc95][Serenity] Baseline code 20230228 (#687)

---
+++ src/migrations/1677553653180-add-column-smart-contract-code-table.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class addColumnSmartContractCodeTable1677553653180
+  implements MigrationInterface
+{
+  name = 'addColumnSmartContractCodeTable1677553653180';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` ADD \`contract_verification\` varchar(255) NOT NULL DEFAULT 'UNVERIFIED'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` ADD \`compiler_version\` varchar(255) NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` ADD \`url\` varchar(255) NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` ADD \`verified_at\` timestamp NULL`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` DROP COLUMN \`verified_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` DROP COLUMN \`url\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` DROP COLUMN \`compiler_version\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`smart_contract_codes\` DROP COLUMN \`contract_verification\``,
+    );
+  }
+}
--- src/shared/entities/smart-contract-code.entity.ts
+++ src/shared/entities/smart-contract-code.entity.ts
@@ -17,4 +17,34 @@ export class SmartContractCode extends BaseEntityIncrementId {
 
   @Column({ name: 'tx_hash', nullable: true })
   tx_hash: string;
+
+  @Column({ name: 'instantiate_msg_schema', type: 'text', nullable: true })
+  instantiate_msg_schema: string;
+
+  @Column({ name: 'query_msg_schema', type: 'text', nullable: true })
+  query_msg_schema: string;
+
+  @Column({ name: 'execute_msg_schema', type: 'text', nullable: true })
+  execute_msg_schema: string;
+
+  @Column({ name: 'contract_hash', nullable: true })
+  contract_hash: string;
+
+  @Column({ name: 's3_location', nullable: true })
+  s3_location: string;
+
+  @Column({ name: 'contract_verification', nullable: true })
+  contract_verification: string;
+
+  @Column({ name: 'compiler_version', nullable: true })
+  compiler_version: string;
+
+  @Column({ name: 'url', nullable: true })
+  url: string;
+
+  @Column({
+    type: 'timestamp',
+    nullable: true,
+  })
+  verified_at: Date;
 }
