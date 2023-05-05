0399437ba8fbf784774767621533bfba1166b45e
nthduc95 <nthduc95@gmail.com>
2023-05-05 10:14:53 +0700

    add check empty object

--- src/components/contract/services/contract.service.ts
+++ src/components/contract/services/contract.service.ts
@@ -490,7 +490,7 @@ export class ContractService {
       return null;
     }
 
-    if (!token?.ipfs) {
+    if (!token?.ipfs || token?.ipfs === '{}') {
       // Get ipfs info
       const ipfs = await lastValueFrom(
         this.httpService
