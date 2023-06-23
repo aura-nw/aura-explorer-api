66b79ae0d79de894648de1bfb44ae943c57157b9
duonghb53 <duonghb53@gmail.com>
2023-06-23 11:30:27 +0700

    remove unused

--- src/components/user/dtos/login.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-export class LoginUserDto {
-  @ApiProperty({ description: 'User email', example: 'example@gmail.com' })
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-}
