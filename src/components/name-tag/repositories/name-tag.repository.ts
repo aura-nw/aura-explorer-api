e004c7d956e55d6ea9a9cf29607894b0f1a1708e
hoangtv49 <codyhoang.it@gmail.com>
2023-06-26 14:16:00 +0700

    feat: search name remove search many name tag

--- src/components/name-tag/repositories/name-tag.repository.ts
+++ src/components/name-tag/repositories/name-tag.repository.ts
@@ -68,15 +68,11 @@ export class NameTagRepository extends Repository<NameTag> {
       .limit(Number(limit) || PAGE_REQUEST.MAX_200);
 
     if (keyword?.length == 1) {
-      qb = qb
-        .where('name_tag LIKE :name_tag ', {
-          name_tag: `%${keyword[0]}%`,
-        })
-        .orWhere('address LIKE :address', { address: `%${keyword[0]}%` });
+      qb = qb.where('name_tag LIKE :name_tag ', {
+        name_tag: `%${keyword[0]}%`,
+      });
     } else if (keyword?.length > 1) {
-      qb = qb
-        .where('address IN(:...addresses)', { addresses: keyword })
-        .orWhere('name_tag IN(:...name_tags)', { name_tags: keyword });
+      qb = qb.where('address IN(:...addresses)', { addresses: keyword });
     }
 
     if (nextKey) {
