84559011aa4d2877d7314cbad28c097bc5bfdddb
hoangtv49 <codyhoang.it@gmail.com>
2023-07-11 15:24:17 +0700

    feat: add login with password

--- src/auth/password/password-auth.module.ts
+++ src/auth/password/password-auth.module.ts
@@ -3,9 +3,10 @@ import { PasswordAuthController } from './password-auth.controller';
 import { Module } from '@nestjs/common';
 import { PasswordAuthService } from './password-auth.service';
 import { MailService } from '../../components/mail/mail.service';
+import { JwtAuthModule } from '../jwt/jwt-auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtAuthModule],
   controllers: [PasswordAuthController],
   providers: [PasswordAuthService, MailService],
 })
