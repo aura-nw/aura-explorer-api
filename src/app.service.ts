b6065ff209b28e1d15eeae0a0af106932797ab8c
nthduc95 <nthduc95@gmail.com>
2023-07-28 15:20:17 +0700

    remove unused

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 import { AkcLogger, RequestContext } from './shared';
-import * as appConfig from './shared/configs/configuration';
 
 @Injectable()
 export class AppService {
-  cosmosScanAPI: string;
-  private appParams;
-
   constructor(private readonly logger: AkcLogger) {
     this.logger.setContext(AppService.name);
-    this.appParams = appConfig.default();
-    this.cosmosScanAPI = this.appParams.cosmosScanAPI;
   }
   getHello(): string {
     const ctx = new RequestContext();
