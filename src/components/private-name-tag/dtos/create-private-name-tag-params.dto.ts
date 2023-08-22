7a6b724b0336e1f4d31febbc1cde30eac395c48d
duonghb53 <duonghb53@gmail.com>
2023-08-22 09:30:46 +0700

    add validate type create/update dto

--- src/components/private-name-tag/dtos/create-private-name-tag-params.dto.ts
+++ src/components/private-name-tag/dtos/create-private-name-tag-params.dto.ts
@@ -1,10 +1,16 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { StorePublicNameTagParamsDto } from '../../public-name-tag/dtos/store-public-name-tag-params.dto';
+import { NAME_TAG_TYPE } from '../../../shared/constants/common';
+import { IsEnum } from 'class-validator';
 
 export class CreatePrivateNameTagParamsDto extends OmitType(
   StorePublicNameTagParamsDto,
   ['id', 'enterpriseUrl'] as const,
 ) {
+  @ApiProperty({ default: '' })
+  @IsEnum(NAME_TAG_TYPE)
+  type: NAME_TAG_TYPE;
+
   @ApiProperty({ default: null })
   note: string;
 }
