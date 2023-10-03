e11705cb85d3a8a6d52c88f5e0ab9a6d25af1024
Maintain <duonghb53@users.noreply.github.com>
2023-10-03 09:17:40 +0700

    Baseline/euphoria 20231002 (#941)
    * Baseline/sererity 20230925 (#936)
    
    * [[Private Name Tag] Limited - Squash and merge to develop (#938) (#939)
    
    * add check limited private name tag
    
    * fix litmit private name tag & remove api unused
    
    * change message limit from env
    
    * Baseline/serenity 20230926 (#937)
    
    [Public-name-tag][CW20] Enhance admin nametag
    
    * fix comment
    
    ---------
    
    Co-authored-by: Duc Nguyen <119287881+nthduc95@users.noreply.github.com>

--- src/components/public-name-tag/dtos/update-public-name-tag-params.dto.ts
+++ src/components/public-name-tag/dtos/update-public-name-tag-params.dto.ts
@@ -2,15 +2,9 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { NAME_TAG_TYPE } from '../../../shared';
 import { IsOptional, MaxLength } from 'class-validator';
 
-export class StorePublicNameTagParamsDto {
+export class UpdatePublicNameTagParamsDto {
   @ApiProperty({ default: '' })
   id: number;
-
-  @ApiProperty({ default: '' })
-  type: NAME_TAG_TYPE;
-
-  @ApiProperty({ default: '' })
-  address: string;
 
   @ApiProperty({ default: '' })
   @MaxLength(35)
