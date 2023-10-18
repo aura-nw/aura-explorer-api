e94232f2c87d00768d3458e6820069e5b9e3e01f
hoangtv49 <92872128+hoangtv49@users.noreply.github.com>
2023-10-18 10:26:22 +0700

    fix: get original ip for throttler (#965)

--- src/shared/guards/custom-csv-throttler-guard.ts
+++ src/shared/guards/custom-csv-throttler-guard.ts
@@ -7,4 +7,8 @@ export class CustomCsvThrottlerGuard extends ThrottlerGuard {
     'You have reached the limit for the number of consecutive data exports, please try again after ' +
     Math.floor(Number(process.env.LIMITER_CSV_TTL || 300000) / 60000) +
     ' minutes.';
+
+  protected async getTracker(req: Record<string, any>): Promise<string> {
+    return req.ips.length ? req.ips[0] : req.ip;
+  }
 }
