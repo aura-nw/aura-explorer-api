26bebbd890ccbf2cc2db7c8edd72e4576f2ec9d0
hoangtv49 <92872128+hoangtv49@users.noreply.github.com>
2023-11-07 14:35:48 +0700

    fix: validate watch list (#1003)

--- src/components/watch-list/dto/create-watch-list.dto.ts
+++ src/components/watch-list/dto/create-watch-list.dto.ts
@@ -2,17 +2,12 @@ import {
   ApiProperty,
   ApiPropertyOptional,
 } from '@nestjs/swagger/dist/decorators';
-import {
-  IsBoolean,
-  IsIn,
-  IsJSON,
-  IsNotEmpty,
-  IsOptional,
-} from 'class-validator';
+import { IsBoolean, IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { NAME_TAG_TYPE, WATCH_LIST } from '../../../shared';
 import { IsValidBench32Address } from '../validators/validate-address';
 import { User } from '../../../shared/entities/user.entity';
 import { MatchKeys } from '../validators/match-keys';
+import { MatchType } from '../validators/match-type';
 export class CreateWatchListDto {
   @ApiProperty()
   @IsNotEmpty()
@@ -21,6 +16,7 @@ export class CreateWatchListDto {
 
   @ApiProperty({ default: NAME_TAG_TYPE.ACCOUNT })
   @IsIn([NAME_TAG_TYPE.ACCOUNT, NAME_TAG_TYPE.CONTRACT])
+  @MatchType('address')
   type: NAME_TAG_TYPE;
 
   @ApiPropertyOptional()
