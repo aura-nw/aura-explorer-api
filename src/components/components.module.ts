ec2eae76f5dc7c4a020a656f019ede755824705a
Duc Nguyen <nthduc95@gmail.com>
2023-11-27 13:29:19 +0700

    remove influxdb

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SharedModule } from '../shared/shared.module';
-import { MetricModule } from './metric/metric.module';
 
 @Module({
-  imports: [SharedModule, MetricModule, ScheduleModule.forRoot()],
+  imports: [SharedModule, ScheduleModule.forRoot()],
 })
 export class ComponentsModule {}
