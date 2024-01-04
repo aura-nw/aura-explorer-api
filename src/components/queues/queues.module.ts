cc04671d9682af6491ce5824dca84c1dd2f5fd1c
Duc Nguyen <119287881+nthduc95@users.noreply.github.com>
2024-01-04 15:33:25 +0700

    [DEV][COIN_TOKEN] revert sync coin token  (#1068)
    (cherry picked from commit 14c3dcf1263903da62c8071fc53929937fa3f9f3)

--- src/components/queues/queues.module.ts
+++ src/components/queues/queues.module.ts
@@ -2,6 +2,7 @@ import { Injectable, Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { ConfigService } from '@nestjs/config';
 import { SendMailModule } from './send-mail/send-mail.module';
+import { TokenModule } from './token/token.module';
 import { CW4973QueueModule } from './cw4973/cw4973.module';
 import { NotificationModule } from './notification/notification.module';
 
@@ -25,6 +26,7 @@ import { NotificationModule } from './notification/notification.module';
       inject: [ConfigService],
     }),
     SendMailModule,
+    TokenModule,
     CW4973QueueModule,
     NotificationModule,
   ],
