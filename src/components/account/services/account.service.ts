a04d709332b9e9e0abb42ec29932fe185f6e9aed
Duc Nguyen <119287881+nthduc95@users.noreply.github.com>
2024-01-22 14:59:12 +0700

    [TOKEN_HOLDER][DEV] Implement query comos get data holder  (#1080)
    (cherry picked from commit 2179d9bd2553d1568c15836384b4138e617ca375)

--- src/components/account/services/account.service.ts
+++ src/components/account/services/account.service.ts
@@ -2,8 +2,28 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { ServiceUtil } from '../../../shared/utils/service.util';
 
 import * as util from 'util';
-import { AkcLogger, INDEXER_API_V2, RequestContext } from '../../../shared';
+import {
+  AkcLogger,
+  INDEXER_API_V2,
+  LIMIT_HOLDER_ADDRESS,
+  RPC_QUERY_URL,
+  RequestContext,
+} from '../../../shared';
 import * as appConfig from '../../../shared/configs/configuration';
+import {
+  QueryDelegatorDelegationsRequest,
+  QueryDelegatorDelegationsResponse,
+  QueryDelegatorUnbondingDelegationsRequest,
+  QueryDelegatorUnbondingDelegationsResponse,
+} from 'cosmjs-types/cosmos/staking/v1beta1/query';
+
+import {
+  QueryDelegationTotalRewardsRequest,
+  QueryDelegationTotalRewardsResponse,
+  QueryValidatorCommissionRequest,
+  QueryValidatorCommissionResponse,
+} from 'cosmjs-types/cosmos/distribution/v1beta1/query';
+import { TransactionHelper } from '../../../shared/helpers/transaction.helper';
 
 @Injectable()
 export class AccountService {
@@ -25,6 +45,7 @@ export class AccountService {
     this.decimals = appParams.chainInfo.coinDecimals;
     this.chainDB = appParams.indexerV2.chainDB;
   }
+
   changeUauraToAura(amount) {
     return (amount / this.precisionDiv).toFixed(this.decimals);
   }
@@ -200,6 +221,264 @@ export class AccountService {
       this.logger.error(
         ctx,
         `Class ${AccountService.name} call ${this.getTotalBalanceByAddress.name} error ${err?.code} method error: ${err?.stack}`,
+      );
+      throw new BadRequestException(err);
+    }
+  }
+
+  async getTotalBalanceByListAddress(
+    ctx: RequestContext,
+    address: string[],
+  ): Promise<any> {
+    this.logger.log(
+      ctx,
+      `${this.getTotalBalanceByListAddress.name} was called!`,
+    );
+    try {
+      if (address.length > LIMIT_HOLDER_ADDRESS) {
+        throw new BadRequestException(
+          `You have reached out of ${LIMIT_HOLDER_ADDRESS} max limitation of address.`,
+        );
+      }
+
+      // get list account detail
+      const graphqlQueryAcc = {
+        query: util.format(INDEXER_API_V2.GRAPH_QL.LIST_ACCOUNT),
+        variables: {
+          address: address,
+        },
+        operationName: INDEXER_API_V2.OPERATION_NAME.LIST_ACCOUNT,
+      };
+
+      const graphqlQueryVal = {
+        query: INDEXER_API_V2.GRAPH_QL.LIST_VALIDATOR,
+        variables: {
+          address: address,
+        },
+        operationName: INDEXER_API_V2.OPERATION_NAME.LIST_VALIDATOR,
+      };
+
+      const [account, validators, delegationsResponse, unbondingResponse] =
+        await Promise.all([
+          this.serviceUtil.fetchDataFromGraphQL(graphqlQueryAcc),
+          this.serviceUtil.fetchDataFromGraphQL(graphqlQueryVal),
+          this.getDelegatorDelegations(address),
+          this.getDelegatorUnbondingDelegations(address),
+        ]);
+
+      const accountData = account?.data[this.chainDB]['account'] || [];
+      const result = [];
+      for (const data of accountData) {
+        const validatorData = validators?.data[this.chainDB]['validator'];
+        if (!data) {
+          return { address, amount: 0 };
+        }
+
+        const accountDelegations = delegationsResponse?.find((item) => {
+          return !!item.find(
+            (el) => el.delegation.delegatorAddress === data.address,
+          );
+        });
+
+        const unbondingDelegations = unbondingResponse?.find((item) => {
+          return !!item.find((el) => el.delegatorAddress === data.address);
+        });
+
+        // get balance
+        let balancesAmount = 0;
+        const balances = data.balances?.length ? data.balances : [];
+        balances.forEach((item) => {
+          if (item.denom === this.minimalDenom) {
+            balancesAmount = parseFloat(item.amount);
+          }
+        });
+
+        // Get available
+        let available = 0;
+        if (data?.spendable_balances) {
+          const spendable_balances = data.spendable_balances?.length
+            ? data.spendable_balances
+            : [];
+          const uaura = spendable_balances?.find(
+            (f) => f.denom === this.minimalDenom,
+          );
+          if (uaura) {
+            const amount = uaura.amount;
+            available = parseFloat(amount);
+          }
+        }
+
+        // Get delegate
+        let delegatedAmount = 0;
+        let stakeReward = 0;
+        if (accountDelegations?.length > 0) {
+          const delegationsRewardRespone =
+            await this.queryDelegationTotalRewardsRequests(data.address);
+          accountDelegations.forEach((item) => {
+            delegatedAmount += parseInt(item.balance.amount);
+            if (
+              delegationsRewardRespone &&
+              delegationsRewardRespone.length > 0 &&
+              delegationsRewardRespone[0].denom === this.minimalDenom
+            ) {
+              stakeReward = parseInt(delegationsRewardRespone[0].amount);
+            }
+          });
+        }
+
+        // get unbonding
+        let unbondingAmount = 0;
+
+        unbondingDelegations?.forEach((item) => {
+          item.entries?.forEach((item1) => {
+            unbondingAmount += parseInt(item1.balance);
+          });
+        });
+
+        // get commission
+        let commission = '0';
+        // get validator by delegation address
+        const validator = validatorData?.filter(
+          (e) => e.account_address === data.address,
+        );
+
+        if (validator?.length > 0) {
+          const commissionData = await this.queryValidatorCommissionRequests(
+            validator[0].operator_address,
+          );
+          if (
+            commissionData?.length > 0 &&
+            commissionData[0].denom === this.minimalDenom
+          ) {
+            commission = commissionData[0].amount;
+          }
+        }
+
+        // //get auth_info
+        let delegatedVesting = 0;
+        if (balancesAmount > 0) {
+          delegatedVesting = balancesAmount - available;
+        }
+        // get total
+        const total =
+          available +
+          delegatedAmount +
+          unbondingAmount +
+          TransactionHelper.balanceOf(stakeReward, 18) +
+          TransactionHelper.balanceOf(commission, 18) +
+          delegatedVesting;
+
+        result.push({ address: data.address, amount: total });
+      }
+      return result;
+    } catch (err) {
+      this.logger.error(
+        ctx,
+        `Class ${AccountService.name} call ${this.getTotalBalanceByListAddress.name} error ${err?.code} method error: ${err?.stack}`,
+      );
+      throw new BadRequestException(err);
+    }
+  }
+
+  async getDelegatorDelegations(address = []) {
+    const res = address.map((delegatorAddr) =>
+      this.queryDelegatorDelegationsRequests(delegatorAddr),
+    );
+    const results = await Promise.allSettled(res);
+    return results
+      .filter((x) => x.status === 'fulfilled')
+      .map((x: any) => x.value);
+  }
+
+  async queryDelegatorDelegationsRequests(delegatorAddr: string) {
+    try {
+      const response = await this.serviceUtil.queryComosRPC(
+        RPC_QUERY_URL.DELEGATOR_DELEGATIONS,
+        QueryDelegatorDelegationsRequest.encode({
+          delegatorAddr,
+        }).finish(),
+      );
+      const value = response.result.response.value;
+      return QueryDelegatorDelegationsResponse.decode(
+        Buffer.from(value, 'base64'),
+      ).delegationResponses;
+    } catch (err) {
+      this.logger.error(
+        null,
+        `Class ${AccountService.name} call ${this.queryDelegatorDelegationsRequests.name} error ${err?.code} method error: ${err?.stack}`,
+      );
+      throw new BadRequestException(err);
+    }
+  }
+
+  async getDelegatorUnbondingDelegations(address = []) {
+    const res = address.map((delegatorAddr) =>
+      this.queryDelegatorUnbondingDelegationsRequests(delegatorAddr),
+    );
+    const results = await Promise.allSettled(res);
+    return results
+      .filter((x) => x.status === 'fulfilled')
+      .map((x: any) => x.value);
+  }
+
+  async queryDelegatorUnbondingDelegationsRequests(delegatorAddr: string) {
+    try {
+      const response = await this.serviceUtil.queryComosRPC(
+        RPC_QUERY_URL.DELEGATOR_UNBONDING_DELEGATIONS,
+        QueryDelegatorUnbondingDelegationsRequest.encode({
+          delegatorAddr,
+        }).finish(),
+      );
+      const value = response.result.response.value;
+      return QueryDelegatorUnbondingDelegationsResponse.decode(
+        Buffer.from(value, 'base64'),
+      ).unbondingResponses;
+    } catch (err) {
+      this.logger.error(
+        null,
+        `Class ${AccountService.name} call ${this.queryDelegatorUnbondingDelegationsRequests.name} error ${err?.code} method error: ${err?.stack}`,
+      );
+      throw new BadRequestException(err);
+    }
+  }
+
+  async queryDelegationTotalRewardsRequests(delegatorAddress: string) {
+    try {
+      const response = await this.serviceUtil.queryComosRPC(
+        RPC_QUERY_URL.DELEGATION_TOTAL_REWARDS,
+        QueryDelegationTotalRewardsRequest.encode({
+          delegatorAddress,
+        }).finish(),
+      );
+      const value = response.result.response.value;
+      return QueryDelegationTotalRewardsResponse.decode(
+        Buffer.from(value, 'base64'),
+      ).total;
+    } catch (err) {
+      this.logger.error(
+        null,
+        `Class ${AccountService.name} call ${this.queryDelegationTotalRewardsRequests.name} error ${err?.code} method error: ${err?.stack}`,
+      );
+      throw new BadRequestException(err);
+    }
+  }
+
+  async queryValidatorCommissionRequests(validatorAddress: string) {
+    try {
+      const response = await this.serviceUtil.queryComosRPC(
+        RPC_QUERY_URL.VALIDATOR_COMMISSION,
+        QueryValidatorCommissionRequest.encode({
+          validatorAddress,
+        }).finish(),
+      );
+      const value = response.result.response.value;
+      return QueryValidatorCommissionResponse.decode(
+        Buffer.from(value, 'base64'),
+      ).commission?.commission;
+    } catch (err) {
+      this.logger.error(
+        null,
+        `Class ${AccountService.name} call ${this.queryValidatorCommissionRequests.name} error ${err?.code} method error: ${err?.stack}`,
       );
       throw new BadRequestException(err);
     }
